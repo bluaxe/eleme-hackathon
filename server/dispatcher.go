d16362bb702bf65557c9348fa4035ad51240179b
axe <axe@sjtu.edu.cn>
2015-11-14 00:28:06 +0800

    change tests

--- server/dispatcher.go
+++ server/dispatcher.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"persist"
 )
 
 type handler func(url.Values, http.ResponseWriter)
@@ -12,10 +14,27 @@ var entrys map[string]handler = make(map[string]handler)
 
 func init() {
 	entrys["test"] = testfunc
+	entrys["forbid"] = testforbid
+	entrys["dbtest"] = testdb
 }
 
 func testfunc(v url.Values, w http.ResponseWriter) {
 	fmt.Fprintf(w, "test func\n")
+}
+
+func testdb(v url.Values, w http.ResponseWriter) {
+	fmt.Fprintf(w, persist.List10User())
+}
+
+func testforbid(v url.Values, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusForbidden)
+	res := &Response{
+		status: http.StatusForbidden,
+		Msg:    "forbidden",
+	}
+
+	str, _ := json.Marshal(res)
+	fmt.Fprintf(w, string(str))
 }
 
 func dispatcher(w http.ResponseWriter, r *http.Request) {
--- server/server.go
+++ server/server.go
@@ -1,14 +1,23 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Start(addr string) {
 	http.HandleFunc("/", dispatcher)
+	http.HandleFunc("/login", loginDispatcher)
+	http.HandleFunc("/foods", foodsDispatcher)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func writeResponse(w http.ResponseWriter, r *Response) {
+	w.WriteHeader(r.status)
+	ret, _ := json.Marshal(r)
+	fmt.Fprintf(w, string(ret))
 }
