6a1725f29ec590b0c9b8316dbc03604929c8a661
axe <axe@sjtu.edu.cn>
2015-11-15 00:35:02 +0800

    merge new tests and benchmark

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,9 +11,8 @@ def test_login_success(url, username, password):
         data=json.dumps(data),
         headers={"Content-type": "application/json"},
     )
-
     assert res.status_code == 200
-    assert len(res.json().get("access_token", "")) == 32
+    assert len(res.json().get("access_token", "")) > 0
 
 
 def test_login_error_password(url, username):
