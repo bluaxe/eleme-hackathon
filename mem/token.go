0d330fcf79f228ee531c54007597772ddd2f7178
axe <axe@sjtu.edu.cn>
2015-11-29 12:47:12 +0800

    no token map

--- mem/token.go
+++ mem/token.go
@@ -1,6 +1,7 @@
 package mem
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -8,22 +9,38 @@ var token_map_lock sync.Mutex
 var uid_token_map_lock sync.Mutex
 
 func SaveToken(token string, id int) {
-	token_map_lock.Lock()
-	defer token_map_lock.Unlock()
-	token_map[token] = id
+	/*
+		token_map_lock.Lock()
+		defer token_map_lock.Unlock()
+		token_map[token] = id
+	*/
 }
 
-func GetToken(token string) (int, bool) {
-	id, ok := token_map[token]
-	return id, ok
+func GetToken(token string) (uid int, ok bool) {
+	defer func() {
+		if r := recover(); r != nil {
+			ok = false
+		}
+	}()
+	fmt.Sscanf(token, "%d", &uid)
+	return uid >> 2, true
+	/*
+		id, ok := token_map[token]
+		return id, ok
+	*/
 }
 
 func UserGetToken(uid int) string {
-	return uid_token_map[uid]
+	return fmt.Sprintf("%d", uid<<2)
+	/*
+		return uid_token_map[uid]
+	*/
 }
 
 func UserSetToken(uid int, token string) {
-	uid_token_map_lock.Lock()
-	defer uid_token_map_lock.Unlock()
-	uid_token_map[uid] = token
+	/*
+		uid_token_map_lock.Lock()
+		defer uid_token_map_lock.Unlock()
+		uid_token_map[uid] = token
+	*/
 }
